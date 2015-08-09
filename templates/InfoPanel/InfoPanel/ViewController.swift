16067094196614278af7f31b051f194f151f7814
Patrick Perini <pcperini@gmail.com>
2015-08-08 18:03:33 -0700

    Initial Commit

---
+++ templates/InfoPanel/InfoPanel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfoPanel
+//
+//  Created by PATRICK PERINI on 8/8/15.
+//  Copyright (c) 2015 AppCookbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
